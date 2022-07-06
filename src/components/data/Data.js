6e61b4b88503a1883432242ddb03490491169e21
_Renzo Figueroa_ <_[figuenzo98@gmail.com](mailto:figuenzo98@gmail.com)_>
2022-07-06 11:13:40 -0400

    first commit

---
+++ src/components/data/Data.js
@@ -0,0 +1,318 @@
+export const nav = [
+    {
+      text: "home",
+      path: "/",
+    },
+    {
+      text: "about",
+      path: "/about",
+    },
+    {
+      text: "services",
+      path: "/services",
+    },
+    {
+      text: "blog",
+      path: "/blog",
+    },
+    {
+      text: "pricing",
+      path: "/pricing",
+    },
+    {
+      text: "contact",
+      path: "/contact",
+    },
+  ]
+  export const featured = [
+    {
+      cover: "../images/hero/h1.png",
+      name: "Family House",
+      total: "122 Property",
+    },
+    {
+      cover: "../images/hero/h2.png",
+      name: "House & Villa",
+      total: "155 Property",
+    },
+    {
+      cover: "../images/hero/h3.png",
+      name: "Apartment",
+      total: "300 Property",
+    },
+    {
+      cover: "../images/hero/h4.png",
+      name: "Office & Studio",
+      total: "80 Property",
+    },
+    {
+      cover: "../images/hero/h6.png",
+      name: "Villa & Condo",
+      total: "80 Property",
+    },
+  ]
+  export const list = [
+    {
+      id: 1,
+      cover: "../images/list/p-1.png",
+      name: "Tambem Real Estate",
+      location: "435 Vegas Street, Las Vegas",
+      category: "For Rent",
+      price: "$4,000",
+      type: "Apartment",
+    },
+    {
+      id: 2,
+      cover: "../images/list/p-2.png",
+      name: "Paramount Properties",
+      location: "344 Clermont Road, Florida",
+      category: "For Sale",
+      price: "$8,150",
+      type: "Condos",
+    },
+    {
+      id: 3,
+      cover: "../images/list/p-7.png",
+      name: "House Hunters",
+      location: "3224 Washington, Seattle",
+      category: "For Rent",
+      price: "$3,200",
+      type: "Offices",
+    },
+    {
+      id: 4,
+      cover: "../images/list/p-4.png",
+      name: "Springsfield Realty",
+      location: "4340 Massuchusetts, Boston",
+      category: "For Sale",
+      price: "$5,500",
+      type: "Homes & Villas",
+    },
+    {
+      id: 5,
+      cover: "../images/list/p-5.png",
+      name: "Willshire Realty",
+      location: "437 Haven Road, Denver",
+      category: "For Rent",
+      price: "$3,000",
+      type: "Commercial",
+    },
+    {
+      id: 6,
+      cover: "../images/list/p-6.png",
+      name: "Home Comfort Realty",
+      location: "105 Sherman Road, Maine",
+      category: "For Sale",
+      price: "$4,200",
+      type: "Apartment",
+    },
+  ]
+  export const awards = [
+    {
+      icon: <i class='fa-solid fa-trophy'></i>,
+      num: "32 M	",
+      name: "Xeno Rior Award",
+    },
+    {
+      icon: <i class='fa-solid fa-briefcase'></i>,
+      num: "43 M",
+      name: "Mimo X11 Award",
+    },
+    {
+      icon: <i class='fa-solid fa-lightbulb'></i>,
+      num: "51 M",
+      name: "Australian UGC Award",
+    },
+    {
+      icon: <i class='fa-solid fa-heart'></i>,
+      num: "42 M",
+      name: "IITCA Green Award",
+    },
+  ]
+  export const location = [
+    {
+      id: 1,
+      name: "New Orleans, Louisiana",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-1.png",
+    },
+    {
+      id: 2,
+      name: "Jerrsy, United State",
+      Villas: "12 Villas",
+      Apartments: "10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-2.png",
+    },
+    {
+      id: 3,
+      name: "Liverpool, London",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-3.png",
+    },
+    {
+      id: 4,
+      name: "NewYork, United States",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-4.png",
+    },
+    {
+      id: 5,
+      name: "Montreal, Canada",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-5.png",
+    },
+    {
+      id: 6,
+      name: "California, USA",
+      Villas: "12 Villas",
+      Apartments: " 10 Apartments",
+      Offices: "07 Offices",
+      cover: "./images/location/city-6.png",
+    },
+  ]
+  export const team = [
+    {
+      list: "50",
+      cover: "../images/customer/team-1.jpg",
+      address: "Miami, FL",
+      name: "Alex Rios",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "70",
+      cover: "../images/customer/team-2.jpg",
+      address: "Houston, Texas",
+      name: "Dennis Raid",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "80",
+      cover: "../images/customer/team-3.jpg",
+      address: "Buffalo, New York",
+      name: "Sue Greer",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "51",
+      cover: "../images/customer/team-4.jpg",
+      address: "2272 Briarwood Drive",
+      name: "Stacy Beck",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "42",
+      cover: "../images/customer/team-5.jpg",
+      address: "2272 Briarwood Drive",
+      name: "Harold J. White",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+    {
+      list: "38",
+      cover: "../images/customer/team-6.jpg",
+      address: "Montreal, USA",
+      name: "Matthew Fernandez",
+      icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+    },
+  ]
+  export const price = [
+    {
+      plan: "Basic",
+      price: "29",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "120GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "5GB Cloud Storage",
+        },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+        { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+      ],
+    },
+    {
+      best: "Best Value",
+      plan: "Standard",
+      price: "49",
+      ptext: "per user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "99.5% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "150GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "10GB Cloud Storage",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          change: "color",
+          icon: <i class='fa-solid fa-x'></i>,
+          text: "Enterprise SLA",
+        },
+      ],
+    },
+    {
+      plan: "Platinum",
+      price: "79",
+      ptext: "2 user, per month",
+      list: [
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "100% Uptime Guarantee",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "200GB CDN Bandwidth",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "20GB Cloud Storage",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Personal Help Support",
+        },
+        {
+          icon: <i class='fa-solid fa-check'></i>,
+          text: "Enterprise SLA",
+        },
+      ],
+    },
+  ]
+  export const footer = [
+    {
+      title: "LAYOUTS",
+      text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+    },
+    {
+      title: "ALL SECTIONS",
+      text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+    },
+    {
+      title: "COMPANY",
+      text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+    },
+  ]
